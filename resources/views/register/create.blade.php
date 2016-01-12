7c286b5a2e12756075cb46aa962c9c7f7822a4d6
amudogxxx <amudog_1@hotmail.com>
2016-01-12 13:42:43 +0800

    去掉onsubmit

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -29,7 +29,7 @@
     </script>
 </head>
 <body>
-<form action="/customer/store" method="post" onsubmit="return validatemobile();">
+<form action="/customer/store" method="post">
     <p>
         <span>输入手机号</span>
         <br>
