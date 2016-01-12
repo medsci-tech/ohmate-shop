e084f6eaf07c6820668f103289a9fa8895c2613f
ming <1851464029@qq.com>
2016-01-12 14:46:35 +0800

    delete middleware from education

--- app/Http/Controllers/EductionController.php
+++ app/Http/Controllers/EductionController.php
@@ -9,13 +9,11 @@ use App\Http\Controllers\Controller;
 
 class EductionController extends Controller
 {
-    //
-    function __construct()
-    {
-        // TODO: Implement __construct() method.
-        $this->middleware('auth.wechat');
-        $this->middleware('auth.access');
-    }
+//    function __construct()
+//    {
+//        $this->middleware('auth.wechat');
+//        $this->middleware('auth.access');
+//    }
 
     public function essay()
     {
