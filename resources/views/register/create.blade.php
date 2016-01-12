ec849872212454af2f3a79591a6c9b0c2b5273a3
蔡乾 <cai.q@foxmail.com>
2016-01-12 14:02:44 +0800

    调试

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -29,7 +29,7 @@
     </script>
 </head>
 <body>
-<form action="/customer/store" method="post">
+<form action="{{url('/customer/store')}}" method="post">
     <p>
         <span>输入手机号</span>
         <br>
