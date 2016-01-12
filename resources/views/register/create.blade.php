069eefda5ed391be30022da55940dbd30bfed531
amudogxxx <amudog_1@hotmail.com>
2016-01-12 13:20:26 +0800

    提交注册页面

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,7 +1,43 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ming
- * Date: 2016/1/12
- * Time: 11:48
- */
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>用户注册</title>
+    <script type="text/javascript">
+        function validatemobile() {
+            var mobile = document.getElementById('phone').value;
+            if (mobile.length == 0) {
+                document.getElementById('label').innerText = '请输入手机号码！';
+                document.getElementById('phone').focus();
+                return false;
+            }
+            if (mobile.length != 11) {
+                document.getElementById('label').innerText = '请输入有效的手机号码！';
+                document.getElementById('phone').focus();
+                return false;
+            }
+
+            var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
+            if (!myreg.test(mobile)) {
+                document.getElementById('label').innerText = '请输入有效的手机号码！';
+                document.getElementById('phone').focus();
+                return false;
+            }
+            return true;
+        }
+
+    </script>
+</head>
+<body>
+<form action="/customer/store" method="post" onsubmit="return validatemobile();">
+    <p>
+        <span>输入手机号</span>
+        <br>
+        <input id="phone" name="phone" type="text">
+        <label id="label" for="phone"></label>
+        </label>
+    </p>
+    <p><input type="submit" value="确定"></p>
+</form>
+</body>
+</html>
