ecb8dc748e696d1e0e0b372a019684d689749eee
amudogxxx <amudog_1@hotmail.com>
2016-01-13 11:47:43 +0800

    手机验证码js校验

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -6,26 +6,53 @@
     <script type="text/javascript">
         function validateMobile() {
             var mobile = document.getElementById('phone').value;
+            var code = document.getElementById('code').value;
             if (mobile.length == 0) {
-                document.getElementById('label').innerText = '请输入手机号码！';
+                document.getElementById('label_phone').innerText = '请输入手机号码！';
                 document.getElementById('phone').focus();
                 return false;
             }
             if (mobile.length != 11) {
-                document.getElementById('label').innerText = '请输入有效的手机号码！';
+                document.getElementById('label_phone').innerText = '请输入有效的手机号码！';
                 document.getElementById('phone').focus();
                 return false;
             }
 
             var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/;
             if (!myreg.test(mobile)) {
-                document.getElementById('label').innerText = '请输入有效的手机号码！';
+                document.getElementById('label_phone').innerText = '请输入有效的手机号码！';
                 document.getElementById('phone').focus();
                 return false;
             }
-            return true;
+
         }
 
+        function validateAll() {
+            if (!validateMobile()) {
+                return false;
+            }
+
+            if (code.length == 0) {
+                document.getElementById('label_code').innerText = '请输入验证码！';
+                document.getElementById('code').focus();
+                return false;
+            }
+
+            if (code.length != 6) {
+                document.getElementById('label_code').innerText = '请输入有效的验证码！';
+                document.getElementById('code').focus();
+                return false;
+            }
+
+            if (isNaN(code)) {
+                document.getElementById('label_code').innerText = '请输入有效的验证码！';
+                document.getElementById('code').focus();
+                return false;
+            }
+
+            return true;
+        }
+        
         function turnTo() {
             if (validateMobile()) {
                 var mobile = document.getElementById('phone').value;
@@ -41,7 +68,14 @@
         <span>输入手机号</span>
         <br>
         <input id="phone" name="phone" type="text">
-        <label id="label" for="phone"></label>
+        <label id="label_phone" for="phone"></label>
+        </label>
+    </p>
+    <p>
+        <span>输入验证码</span>
+        <br>
+        <input id="code" name="code" type="text">
+        <label id="label_code" for="code"></label>
         </label>
     </p>
     <p><input type="submit" value="确定"></p>
