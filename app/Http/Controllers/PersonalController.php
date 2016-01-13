c075871fc021504823e59a7df3351edb5998d582
amudogxxx <amudog_1@hotmail.com>
2016-01-13 16:57:41 +0800

    修改用户信息

--- app/Http/Controllers/PersonalController.php
+++ app/Http/Controllers/PersonalController.php
@@ -23,7 +23,26 @@ class PersonalController extends Controller
 
     public function information()
     {
-        return 'information';
+        if (!\Session::has('logged_user')) {
+            return "session no exists";
+        }/*if>*/
+
+        $user = \Session::get('logged_user');
+
+        $customer = Customer::where('openid', $user['openid'])->first();
+        \Log::info('advertisement:' . $customer);
+        if (!$customer) {
+            return redirect('/register/focus');
+        } /*if>*/
+
+        if ((!$customer->phone) || (!$customer->is_registered)) {
+            return redirect('/register/create');
+        } /*if>*/
+
+        $info = '昵称:' . $customer->nickname . ' 头像:' . $customer->headimgurl
+            . ' 电话:' . $customer->phone;
+
+        return 'information '.$info;
     }
 
     public function beans()
@@ -55,8 +74,7 @@ class PersonalController extends Controller
         } /*if>*/
 
         if ($customer->qr_code) {
-            $qrCode = $customer->qr_code;
-            return view('personal.advertisement', ['qrCode' => $qrCode]);
+            return view('personal.advertisement', ['qrCode' => $customer->qr_code]);
         } /*if>*/
 
         return 'advertisement';
