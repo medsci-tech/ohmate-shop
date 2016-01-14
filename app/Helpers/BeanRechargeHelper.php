1c510cfb1d58bcb50358c2d3566960838c772add
ming <1851464029@qq.com>
2016-01-14 16:48:36 +0800

    test

--- app/Helpers/BeanRechargeHelper.php
+++ app/Helpers/BeanRechargeHelper.php
@@ -32,16 +32,21 @@ class BeanRechargeHelper {
     }
 
     public static function inviteFeedback($referrer) {
-        if ($referrer) {
+        \Log::debug('1');
+        if (0 == $referrer) {
+            \Log::debug('2');
             return false;
         } /*if>*/
 
         $customer = Customer::where('id', $referrer)->first();
         if ($customer) {
+            \Log::debug('3');
             return false;
         } /*if>*/
 
+        \Log::debug('4');
         recharge($referrer, AppConstant::BEAN_ACTION_INVITE);
+        \Log::debug('5');
     }
 
 } /*class*/
