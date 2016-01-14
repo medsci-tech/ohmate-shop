6cbed36fe43f88e10fa524e2bfb6a78f6e9fd383
amudogxxx <amudog_1@hotmail.com>
2016-01-14 12:24:47 +0800

    添加查看链接

--- resources/views/register/focus.blade.php
+++ resources/views/register/focus.blade.php
@@ -1,9 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ming
- * Date: 2016/1/13
- * Time: 17:44
- */
-<h1>focus</h1>
-<h1>去关注</h1>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>关于我们</title>
+</head>
+<body>
+
+<a href="http://mp.weixin.qq.com/s?__biz=MzAxMTY0OTc1MQ==&mid=401938638&idx=1&sn=d4483f5087ca8ccd51f48b74a5747f9c#rd">
+    查看详情</a>
+</body>
+</html>
