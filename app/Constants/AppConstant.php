b4464542e8efb50e3521ad1dbb23d409bfa6c2ea
amudogxxx <amudog_1@hotmail.com>
2016-01-14 14:59:22 +0800

    添加关注地址

--- app/Constants/AppConstant.php
+++ app/Constants/AppConstant.php
@@ -37,4 +37,7 @@ class AppConstant {
     const BEAN_ACTION_EDUCATION_FEEDBACK    = 'feedback_doctor_education';
     const BEAN_ACTION_CONSUME_FEEDBACK      = 'feedback_doctor_consume';
 
+    /*url*/
+    const ATTENTION_URL = 'http://mp.weixin.qq.com/s?__biz=MzAxMTY0OTc1MQ==&mid=401938638&idx=1&sn=d4483f5087ca8ccd51f48b74a5747f9c&scene=1&srcid=0114rNGJEWOnEQdEGpnX7Y7i&from=groupmessage&isappinstalled=0&key=41ecb04b051110034d288743b0058d2ba205fe3a1f448de9349ded00996528b67c70fa5a829663e231fb2043c2d4952f&ascene=1&uin=MTE4MTQ5NTM4MA%3D%3D&devicetype=android-19&version=26030849&nettype=WIFI&pass_ticket=XQ1572Eciw7RzuFw3JN%2FcySvJpc4SLsZ5mrM2iG3sEn8Wu1VZF1qnz4K9S1%2Fe%2F7O';
+
 } /*class*/
