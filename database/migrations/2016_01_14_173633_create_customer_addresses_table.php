d35fe9d4d55df53aff79be5249e8111db896be02
ming <1851464029@qq.com>
2016-01-14 17:52:47 +0800

    add customer address

--- database/migrations/2016_01_14_173633_create_customer_addresses_table.php
+++ database/migrations/2016_01_14_173633_create_customer_addresses_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateHospitalsTable extends Migration
+class CreateCustomerAddressesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateHospitalsTable extends Migration
      */
     public function up()
     {
-        Schema::create('hospitals', function (Blueprint $table) {
+        Schema::create('customer_addresses', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
         });
@@ -25,6 +25,6 @@ class CreateHospitalsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('hospitals');
+        Schema::drop('customer_addresses');
     }
 }
