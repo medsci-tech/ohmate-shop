1361f672f826cfd8ceca7662c2d407e0f2f2692d
蔡乾 <cai.q@foxmail.com>
2016-02-17 15:08:55 +0800

    finish hoo

--- app/Http/Controllers/Github/GithubController.php
+++ app/Http/Controllers/Github/GithubController.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Controller;
 class GithubController extends Controller
 {
     public function onEvent() {
-        exec('git pull origin master', $d);
+        exec('sudo git pull origin master', $d);
+        dd($d);
     }
 }
