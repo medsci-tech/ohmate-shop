d1ccaa11f3aec46ac72998040604489c370391d7
蔡乾 <cai.q@foxmail.com>
2016-02-17 16:40:12 +0800

    see category

--- app/Http/Controllers/Shop/CategoryController.php
+++ app/Http/Controllers/Shop/CategoryController.php
@@ -10,6 +10,6 @@ use App\Http\Controllers\Controller;
 class CategoryController extends Controller
 {
     public function index() {
-        return 'category.index';
+        return view('shop.category');
     }
 }
