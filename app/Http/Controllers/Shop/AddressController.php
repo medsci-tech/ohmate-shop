284acd70beb47e419a31936f37afa892ca799c37
蔡乾 <cai.q@foxmail.com>
2016-02-19 15:03:31 +0800

    add address route to blade

--- app/Http/Controllers/Shop/AddressController.php
+++ app/Http/Controllers/Shop/AddressController.php
@@ -10,6 +10,6 @@ use App\Http\Controllers\Controller;
 class AddressController extends Controller
 {
     public function index() {
-        return 'address.index';
+        return view('shop.address');
     }
 }
