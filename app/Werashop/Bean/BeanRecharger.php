1b42121f86a21a5dc8d45502021c241fa1759878
ming <1851464029@qq.com>
2016-02-19 17:54:12 +0800

    add bean recharger

---
+++ app/Werashop/Bean/BeanRecharger.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ming
+ * Date: 2016/2/19
+ * Time: 17:31
+ */
+
+namespace App\Werashop\Bean;
+
+
+class BeanRecharger
+{
+    public function test()
+    {
+        echo 'bean test';
+    }
+
+}
