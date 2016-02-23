eeb1f40135c35f5b8549767ca5ddd46697500d67
Icefly <501254061@qq.com>
2016-02-23 14:53:50 +0800

    bugfix

--- public/js/goods.js
+++ public/js/goods.js
@@ -1,8 +1,8 @@
-//if (localStorage.cart != 'undefined') {
-//  var cart = JSON.parse(localStorage.cart);
-//} else {
-//  var cart = [];
-//}
+if (localStorage.cart != 'undefined') {
+  var cart = JSON.parse(localStorage.cart);
+} else {
+  var cart = [];
+}
 
 
 var list = new Vue({
@@ -15,7 +15,7 @@ var list = new Vue({
         price: 22.00,
         priceBefore: 30.00,
         num: 1
-      }
+      },
 //        goods: {
 //          id: '{{$item->id}}',
 //          name: '{{$item->name}}'.replace("&reg;","®"),
@@ -25,48 +25,48 @@ var list = new Vue({
 //          num: 1
 //         },
 //      cart: cart
-//    },
-//    computed: {
-//      alreadyHave: function () {
-//        for (i = 0; i < this.cart.length; i++) {
-//          if (this.cart[i].id == this.goods.id) {
-//            return i;
-//          }
-//        }
-//        return -1;
-//      }
-//    },
-//
-//    methods: {
-//      addGoods: function () {
-//        if (this.alreadyHave != -1) {
-//          this.cart[this.alreadyHave].num += this.goods.num;
-//        } else {
-//          this.cart.push({
-//            id: this.goods.id,
-//            name: this.goods.name,
-//            tag: this.goods.tag,
-//            price: this.goods.price,
-//            priceBefore: this.goods.priceBefore,
-//            num: this.goods.num
-//          });
-//        }
-//        localStorage.cart = JSON.stringify(this.cart);
-//        this.goods.num = 1;
-//        $('.jumbotron').show();
-//        $('.jumbotron').delay(1000).hide(0);
-//        $('.jumbotron .alert').show();
-//        $('.jumbotron .alert').delay(300).fadeOut(700);
-//      },
-//      numMinus: function () {
-//        if (this.goods.num >= 2) {
-//          this.goods.num--;
-//        }
-//      },
-//      numAdd: function () {
-//        if (this.goods.num <= 98) {
-//          this.goods.num++;
-//        }
-//      }
+    },
+    computed: {
+      alreadyHave: function () {
+        for (i = 0; i < this.cart.length; i++) {
+          if (this.cart[i].id == this.goods.id) {
+            return i;
+          }
+        }
+        return -1;
+      }
+    },
+
+    methods: {
+      addGoods: function () {
+        if (this.alreadyHave != -1) {
+          this.cart[this.alreadyHave].num += this.goods.num;
+        } else {
+          this.cart.push({
+            id: this.goods.id,
+            name: this.goods.name,
+            tag: this.goods.tag,
+            price: this.goods.price,
+            priceBefore: this.goods.priceBefore,
+            num: this.goods.num
+          });
+        }
+        localStorage.cart = JSON.stringify(this.cart);
+        this.goods.num = 1;
+        $('.jumbotron').show();
+        $('.jumbotron').delay(1000).hide(0);
+        $('.jumbotron .alert').show();
+        $('.jumbotron .alert').delay(300).fadeOut(700);
+      },
+      numMinus: function () {
+        if (this.goods.num >= 2) {
+          this.goods.num--;
+        }
+      },
+      numAdd: function () {
+        if (this.goods.num <= 98) {
+          this.goods.num++;
+        }
+      }
     }
-  })
+  });
