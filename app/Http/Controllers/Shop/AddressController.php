b6b84a81b0d0b3ec05c57a7027096e9cc165dc35
蔡乾 <cai.q@foxmail.com>
2016-02-23 13:05:23 +0800

    for debug

--- app/Http/Controllers/Shop/AddressController.php
+++ app/Http/Controllers/Shop/AddressController.php
@@ -30,7 +30,6 @@ class AddressController extends Controller
 
         $customer = \Helper::getCustomer();
 
-        dd($customer->addresses()->get());
         return view('shop.address')->with([
             'items' => $customer->addresses()->get()
         ]);
