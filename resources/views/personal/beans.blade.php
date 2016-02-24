0aa8958a3426d960f4ea53765fb2f4ac2cd6b384
Icefly <501254061@qq.com>
2016-02-24 17:51:21 +0800

    迈豆钱包页面

--- resources/views/personal/beans.blade.php
+++ resources/views/personal/beans.blade.php
@@ -1,7 +1,172 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ming
- * Date: 2016/1/18
- * Time: 10:12
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+  <title>迈豆钱包</title>
+  <link rel="stylesheet" href="../../css/personal.css">
+</head>
+<body>
+<div class="container">
+  <div class="row ">
+    <div id="beans-header">
+      <h4 class="text-center"><span>50</span>迈豆</h4>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-3 text-center">
+      <p>03-24</p>
+    </div>
+    <div class="media col-xs-9">
+      <div class="media-left media-middle">
+        <a href="#">
+          <img class="media-object" src="../../image/shop_goods/7.png" alt="...">
+        </a>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">+2180</h4>
+
+        <p>新用户注册</p>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-3 text-center">
+      <p>03-24</p>
+    </div>
+    <div class="media col-xs-9">
+      <div class="media-left media-middle">
+        <a href="#">
+          <img class="media-object" src="../../image/shop_goods/7.png" alt="...">
+        </a>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">+20</h4>
+
+        <p>每日签到</p>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-3 text-center">
+      <p>03-24</p>
+    </div>
+    <div class="media col-xs-9">
+      <div class="media-left media-middle">
+        <a href="#">
+          <img class="media-object" src="../../image/shop_goods/7.png" alt="...">
+        </a>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">+10</h4>
+
+        <p>浏览文章</p>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-3 text-center">
+      <p>03-24</p>
+    </div>
+    <div class="media col-xs-9">
+      <div class="media-left media-middle">
+        <a href="#">
+          <img class="media-object" src="../../image/shop_goods/7.png" alt="...">
+        </a>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">+20</h4>
+
+        <p>浏览视频</p>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-3 text-center">
+      <p>03-24</p>
+    </div>
+    <div class="media col-xs-9">
+      <div class="media-left media-middle">
+        <a href="#">
+          <img class="media-object" src="../../image/shop_goods/7.png" alt="...">
+        </a>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">+200</h4>
+
+        <p>商城返利</p>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-3 text-center">
+      <p>03-24</p>
+    </div>
+    <div class="media col-xs-9">
+      <div class="media-left media-middle">
+        <a href="#">
+          <img class="media-object" src="../../image/shop_goods/7.png" alt="...">
+        </a>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">+100</h4>
+
+        <p>糖友推广</p>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-3 text-center">
+      <p>03-24</p>
+    </div>
+    <div class="media col-xs-9">
+      <div class="media-left media-middle">
+        <a href="#">
+          <img class="media-object" src="../../image/shop_goods/7.png" alt="...">
+        </a>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">+100</h4>
+
+        <p>糖友消费返利</p>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-3 text-center">
+      <p>03-24</p>
+    </div>
+    <div class="media col-xs-9">
+      <div class="media-left media-middle">
+        <a href="#">
+          <img class="media-object" src="../../image/shop_goods/7.png" alt="...">
+        </a>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">+50</h4>
+
+        <p>糖友学习返利</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-3 text-center">
+      <p>03-24</p>
+    </div>
+    <div class="media col-xs-9">
+      <div class="media-left media-middle">
+        <a href="#">
+          <img class="media-object" src="../../image/shop_goods/7.png" alt="...">
+        </a>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">-2000</h4>
+
+        <p>商城消费抵扣</p>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
