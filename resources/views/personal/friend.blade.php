c37a87e15baf6e3a517cfa3948fab40a64d10b63
amudogxxx <amudog_1@hotmail.com>
2016-02-24 18:02:59 +0800

    fix

--- resources/views/personal/friend.blade.php
+++ resources/views/personal/friend.blade.php
@@ -9,13 +9,13 @@
 </head>
 <body>
 <div class="bianju">
-    <div class="tx">
-        <img src="{{$data['head_image_url']}}" class="img-rounded" style="width: 80px;height: 80px">
-    </div>
-    <div class="bianju">
-        <p>{{$data['nickname']}}</p>
-    </div>
-    <h3><small>请扫描下面的二维码，请扫描下面的二维码请扫描下面的二维码请扫描下面的二维码请</small></h3>
+    {{--<div class="tx">--}}
+        {{--<img src="{{$data['head_image_url']}}" class="img-rounded" style="width: 80px;height: 80px">--}}
+    {{--</div>--}}
+    {{--<div class="bianju">--}}
+        {{--<p>{{$data['nickname']}}</p>--}}
+    {{--</div>--}}
+    <h3><small>请扫描下面的二维码</small></h3>
     <div><img src="{{$data['qrCode']}}" class="img-responsive" style="width: 100%"></div>
 </div>
 </body>
