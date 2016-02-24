d1601044ccfb1824b2f898a916905f3cb84951de
amudogxxx <amudog_1@hotmail.com>
2016-02-24 17:43:23 +0800

    fix

--- resources/views/personal/friend.blade.php
+++ resources/views/personal/friend.blade.php
@@ -1,14 +1,3 @@
-{{--<!DOCTYPE html>--}}
-{{--<html>--}}
-{{--<head lang="en">--}}
-    {{--<meta charset="UTF-8">--}}
-    {{--<title>糖友推广</title>--}}
-{{--</head>--}}
-{{--<body>--}}
-{{--<h1>Hello, <?php echo $qrCode; ?></h1>--}}
-{{--<img src="{{ $qrCode }}" />--}}
-{{--</body>--}}
-{{--</html>--}}
 <!DOCTYPE html>
 <html lang="en">
 <head>
