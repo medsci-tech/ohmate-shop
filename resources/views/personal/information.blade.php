42fc17e5d08dab9e8787c2953b25275655ea8d92
amudogxxx <amudog_1@hotmail.com>
2016-02-25 17:02:18 +0800

    fix

--- resources/views/personal/information.blade.php
+++ resources/views/personal/information.blade.php
@@ -4,65 +4,71 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
     <title>会员信息</title>
-    <link href="http://apps.bdimg.com/libs/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/personal.css" rel="stylesheet">
-    <script type="text/javascript">
-        function goAbout() {
-            window.location.href = '/personal/about-us';
-        }
-    </script>
-</head>
+    <link rel="stylesheet" href="/css/weui.min.css">
+    <link rel="stylesheet" href="/css/personal.css">
 <body>
-<div>
-    <div class="head">
-        <div class="head-img">
-            <img src="{{$data['head_image_url']}}">
-        </div>
-        <div class="head-dsb">
-            <p class="dsb-name">{{$data['nickname']}}</p>
-            <p class="dsb-id">{{$data['type']}}</p>
-        </div>
+<div class="page slideIn cell">
+    <div class="hd img_t">
+        <img src="{{$data['head_image_url']}}" class="img_tx">
     </div>
-    <div class="qianbao">
-        <div class="qianbao_p qianbao_p_1">
-            <p class="maidou_t">迈豆余额</p><br><p class="maidou">{{$data['beans_total']}}迈豆</p>
+    <div class="bd bd_1">
+        <div class="weui_cells">
+            <div class="weui_cell">
+                <div class="weui_cell_bd weui_cell_primary">
+                    <p>账户名</p>
+                </div>
+                <div class="weui_cell_ft">{{$data['nickname']}}</div>
+            </div>
+            <div class="weui_cell">
+                <div class="weui_cell_bd weui_cell_primary">
+                    <p>用户类型</p>
+                </div>
+                <div class="weui_cell_ft">{{$data['type']}}</div>
+            </div>
         </div>
-        <div class="text-center qianbao_p_1">
-            <p class="maidou_t">礼品卡</p><br><p class="maidou">0张</p>
+
+        <div class="weui_cells weui_cells_access">
+            <a href="" class="weui_cell">
+                <div class="weui_cell_bd weui_cell_primary">
+                    <p>迈豆余额</p>
+                </div>
+                <div class="weui_cell_ft text_color">{{$data['beans_total']}}&nbsp;迈豆</div>
+            </a>
+            <a href="" class="weui_cell">
+                <div class="weui_cell_bd weui_cell_primary">
+                    <p>礼品卡</p>
+                </div>
+                <div class="weui_cell_ft text_color">0&nbsp;张</div>
+            </a>
         </div>
-    </div>
-    <div style="margin-top: 20px">
-        <div class="mt-1">
-            <div class="ps-lt">
-                <div class="lt-dsb cl-bb">
+
+        <div class="weui_cells weui_cells_access">
+            <a href="" class="weui_cell">
+                <div class="weui_cell_bd weui_cell_primary">
                     <p>迈豆规则</p>
-                    <i class="jt"></i>
                 </div>
-            </div>
-            <div class="ps-lt">
-                <div class="lt-dsb" onclick="goAbout();">
+                <div class="weui_cell_ft"></div>
+            </a>
+            <a href="/personal/about-us" class="weui_cell">
+                <div class="weui_cell_bd weui_cell_primary">
                     <p>关于我们</p>
-                    <i class="jt"></i>
                 </div>
-            </div>
+                <div class="weui_cell_ft"></div>
+            </a>
         </div>
     </div>
-    {{--<div style="margin-top: 20px">--}}
-        {{--<div class="mt-1">--}}
-            {{--<div class="ps-lt">--}}
-                {{--<div class="lt-dsb" onclick="goAbout();">--}}
-                    {{--<p>关于我们</p>--}}
-                    {{--<i class="jt"></i>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-            {{--<div class="ps-lt">--}}
-                {{--<div class="lt-dsb cl-bb">--}}
-                    {{--<p>联系客服</p>--}}
-                    {{--<i class="jt"></i>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
 </div>
+
 </body>
+<script type="text/javascript">
+    function goAbout() {
+        window.location.href = '/personal/about-us';
+    }
+</script>
+
 </html>
+
+
+
+
+
