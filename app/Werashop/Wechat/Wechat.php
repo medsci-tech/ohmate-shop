4ab03f66f2322a16407bb9ccd9bd083ecef4d28e
蔡乾 <cai.q@foxmail.com>
2016-02-25 17:15:19 +0800

    test

--- app/Werashop/Wechat/Wechat.php
+++ app/Werashop/Wechat/Wechat.php
@@ -98,8 +98,8 @@ class Wechat
             ]),
             (new MenuItem("易康商城"))->buttons([
                 new MenuItem('商城首页', 'view', url('/shop/index')),
-                new MenuItem('我的订单', 'view', url('/shop/orders')),
-                new MenuItem('我的地址', 'view', url('/shop/addresses')),
+                new MenuItem('我的订单', 'view', url('/shop/order')),
+                new MenuItem('我的地址', 'view', url('/shop/address')),
             ]),
             (new MenuItem("个人中心"))->buttons([
                 new MenuItem('会员信息', 'view', url('/personal/information')),
