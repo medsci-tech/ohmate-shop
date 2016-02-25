e61445cdfe95a6acef82389bfabca64574a7fc35
蔡乾 <cai.q@foxmail.com>
2016-02-25 14:26:13 +0800

    bugfix

--- app/Http/Controllers/Shop/AddressController.php
+++ app/Http/Controllers/Shop/AddressController.php
@@ -49,7 +49,7 @@ class AddressController extends Controller
      */
     public function create(Request $request)
     {
-        $item = json_decode($request->all());
+        $item = $request->all();
         dd($item);
     }
 
@@ -58,7 +58,7 @@ class AddressController extends Controller
      */
     public function delete(Request $request)
     {
-        $item = json_decode($request->all());
+        $item = $request->all();
         dd($item);
     }
 
@@ -67,7 +67,7 @@ class AddressController extends Controller
      */
     public function update(Request $request)
     {
-        $item = json_decode($request->all());
+        $item = $request->all();
         dd($item);
     }
 }
