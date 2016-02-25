5669d0c5467d78f58592ce55cc47ad24685cbd12
Icefly <501254061@qq.com>
2016-02-25 17:39:45 +0800

    test

--- resources/views/shop/cart.blade.php
+++ resources/views/shop/cart.blade.php
@@ -17,7 +17,7 @@
 
     <div class="row" v-for="goods in cart">
       <div class="col-xs-3">
-        <img class="img-responsive" src="{{url('/image/shop_goods')}}//@{{ goods.id }}.png" alt="">
+        <img class="img-responsive" src="{{url('/image/shop_goods')}}/" + @{{ goods.id }} + ".png" alt="">
       </div>
       <div class="col-xs-9">
         <h4>@{{ goods.name }}</h4>
