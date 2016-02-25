a10803ee1f7b0865f3e8a9028e081c05cde2fa12
蔡乾 <cai.q@foxmail.com>
2016-02-25 10:42:23 +0800

    add postcode

---
+++ database/migrations/2016_02_25_104058_add_post_code_to_addresses.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPostCodeToAddresses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->string('postcode')->after('phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->dropColumn('postcode');
+        });
+    }
+}
