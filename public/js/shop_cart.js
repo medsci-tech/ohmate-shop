3f9aaed996485a7564a271a119322ed26415ef30
Icefly <501254061@qq.com>
2016-02-25 17:05:52 +0800

    test

--- public/js/shop_cart.js
+++ public/js/shop_cart.js
@@ -4,126 +4,135 @@ if (typeof localStorage.cart != 'undefined') {
   var cart = [];
 }
 
-
 var shop_cart = new Vue({
-    el: '#cart_form',
-    data: {
-      cart: cart,
-      personal: []
-    },
+  el: '#cart_form',
+  data: {
+    cart: cart,
+    beans: 0,
+    address: [],
+    post_fee: 0
+  },
 
-    computed: {
-      cartList: function () {
-        var list = [];
-        for (i = 0; i < this.cart.length; i++) {
-          list.push()
-        }
-        return list;
-      },
-      priceAll: function () {
-        var all = 0;
-        for (i = 0; i < this.cart.length; i++) {
-          all += this.cart[i].price * this.cart[i].num;
-        }
-        return all;
-      },
-      priceDiscount: function () {
-        this.person.consume =
-          this.person.beans < this.priceAll * 100 ? this.person.beans : this.priceAll * 100;
-        return this.person.consume / 100;
-      },
-      priceCount: function () {
-        if (this.address == null) {
-          return this.priceAll + 8 - this.priceDiscount;
-        } else {
-          return this.priceAll + this.address.postage - this.priceDiscount;
-        }
-      },
-      postage: function () {
-        if (this.address == null) {
-          return 8;
-        } else {
-          return this.address.postage;
-        }
+  computed: {
+    cartList: function () {
+      var list = [];
+      for (i = 0; i < this.cart.length; i++) {
+        list.push({
+          id: this.cart[i].id,
+          num: this.cart[i].num
+        })
+      }
+      return list;
+    },
+    priceAll: function () {
+      var all = 0;
+      for (i = 0; i < this.cart.length; i++) {
+        all += this.cart[i].price * this.cart[i].num;
+      }
+      return all;
+    },
+    priceDiscount: function () {
+      var consume =
+        this.personal.beans < this.priceAll * 100 ? this.personal.beans : this.priceAll * 100;
+      return consume / 100;
+    },
+    priceCount: function () {
+      if (this.address == null) {
+        return this.priceAll + 8 - this.priceDiscount;
+      } else {
+        return this.priceAll + this.post_fee - this.priceDiscount;
       }
     },
-
-    methods: {
-      removeGoods: function (e) {
-        this.cart.$remove(e);
-        localStorage.cart = JSON.stringify(this.cart);
-      },
-      priceGoods: function (e) {
-        return e.price * e.num;
-      },
-      numMinus: function (e) {
-        if (e.num >= 2) {
-          e.num--;
-        }
-      },
-      numAdd: function (e) {
-        if (e.num <= 98) {
-          e.num++;
-        }
-      },
-      getPersonal: function () {
-        $.post('/shop/cart/customer-information',{},
-          function(data){
-            if (data.success) {
-              shop_cart.personal = data.data;
-            }
-          },"json"
-        )
-      },
-      postCart: function () {
-        console.log(JSON.stringify(shop_cart.$data));
-        $.post('/shop/order/create',
-          {},
-          function (data) {
-            if (data.success) {
-              function onBridgeReady() {
-                WeixinJSBridge.invoke(
-                  'getBrandWCPayRequest', {
-                    "appId": "wx2421b1c4370ec43b",     //公众号名称，由商户传入
-                    "timeStamp": " 1395712654",         //时间戳，自1970年以来的秒数
-                    "nonceStr": "e61463f8efa94090b1f366cccfbbb444", //随机串
-                    "package": "prepay_id=u802345jgfjsdfgsdg888",
-                    "signType": "MD5",         //微信签名方式：
-                    "paySign": "70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名
-                  },
-                  function (res) {
-                    if (res.err_msg == "get_brand_wcpay_request：ok") {
-                      $.post('/shop/order/create', {"get_brand_wcpay_request": "ok"},
-                        function (data) {
-                          if (data.success){
-                            shop_cart.cart = [];
-                            localStorage.cart.clear();
-                          }
-                        }, "json"
-                      );
-                    }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-                  }
-                );
-              }
-
-              if (typeof WeixinJSBridge == "undefined") {
-                if (document.addEventListener) {
-                  document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-                } else if (document.attachEvent) {
-                  document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
-                  document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-                }
-              } else {
-                onBridgeReady();
-              }
-
-            } else {
-              alert('服务器异常!');
-            }
-          }, "json"
-        );
+    postage: function () {
+      if (this.address == null) {
+        return 8;
+      } else {
+        return this.address.postage;
       }
     }
-  });
+  },
+
+  methods: {
+    removeGoods: function (e) {
+      this.cart.$remove(e);
+      localStorage.cart = JSON.stringify(this.cart);
+    },
+    priceGoods: function (e) {
+      return e.price * e.num;
+    },
+    numMinus: function (e) {
+      if (e.num >= 2) {
+        e.num--;
+      }
+    },
+    numAdd: function (e) {
+      if (e.num <= 98) {
+        e.num++;
+      }
+    },
+    getPersonal: function () {
+      $.post('/shop/cart/customer-information',
+        {
+          cart: cartList,
+          address_id: this.address.id
+        },
+        function (data) {
+          if (data.success) {
+            shop_cart.beans = data.data.beans;
+            shop_cart.address = data.data.default_address;
+          }
+        }, "json"
+      )
+    },
+    postCart: function () {
+      console.log(JSON.stringify(shop_cart.$data));
+      $.post('/shop/order/create',
+        {},
+        function (data) {
+          if (data.success) {
+            function onBridgeReady() {
+              WeixinJSBridge.invoke(
+                'getBrandWCPayRequest', {
+                  "appId": "wx2421b1c4370ec43b",     //公众号名称，由商户传入
+                  "timeStamp": " 1395712654",         //时间戳，自1970年以来的秒数
+                  "nonceStr": "e61463f8efa94090b1f366cccfbbb444", //随机串
+                  "package": "prepay_id=u802345jgfjsdfgsdg888",
+                  "signType": "MD5",         //微信签名方式：
+                  "paySign": "70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名
+                },
+                function (res) {
+                  if (res.err_msg == "get_brand_wcpay_request：ok") {
+                    $.post('/shop/order/create', {"get_brand_wcpay_request": "ok"},
+                      function (data) {
+                        if (data.success) {
+                          shop_cart.cart = [];
+                          localStorage.cart.clear();
+                        }
+                      }, "json"
+                    );
+                  }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                }
+              );
+            }
+
+            if (typeof WeixinJSBridge == "undefined") {
+              if (document.addEventListener) {
+                document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+              } else if (document.attachEvent) {
+                document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+                document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+              }
+            } else {
+              onBridgeReady();
+            }
+
+          } else {
+            alert('服务器异常!');
+          }
+        }, "json"
+      );
+    }
+  }
+});
 
 shop_cart.getPersonal();
