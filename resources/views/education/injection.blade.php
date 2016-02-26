fdd5dfd416f4456ee9e02c6b9a44f430d6ccebe8
amudogxxx <amudog_1@hotmail.com>
2016-02-26 17:13:35 +0800

    add

--- resources/views/education/injection.blade.php
+++ resources/views/education/injection.blade.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ming
- * Date: 2016/2/23
- * Time: 13:25
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+    <title>注射指导</title>
+</head>
+<body>
+<div>
+    <a href="">
+        <div>
+            <img width="100%" src="../../image/education/pen.png" alt="">
+        </div>
+    </a>
+    <a href="">
+        <div>
+            <img width="100%" src="../../image/education/pin.png" alt="">
+        </div>
+    </a>
+</div>
+
+</body>
+</html>
