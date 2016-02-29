21b158eee66c83239dcb76752813ea5de733219a
蔡乾 <cai.q@foxmail.com>
2016-02-29 10:52:06 +0800

    test notify

--- database/migrations/2016_02_29_102059_add_id_md5_to_orders_table.php
+++ database/migrations/2016_02_29_102059_add_id_md5_to_orders_table.php
@@ -16,7 +16,6 @@ class AddIdMd5ToOrdersTable extends Migration
             $table->string('wx_out_trade_no')->unique()->after('id')->comment('商家订单号');
             $table->string('wx_transaction_id')->nullable()->after('wx_out_trade_no')->comment('微信订单号');
 
-            $table->index('wx_out_trade_no');
             $table->index('wx_transaction_id');
         });
     }
@@ -30,7 +29,6 @@ class AddIdMd5ToOrdersTable extends Migration
     {
         Schema::table('orders', function (Blueprint $table) {
             $table->dropIndex('orders_wx_transaction_id_index');
-            $table->dropIndex('orders_wx_out_trade_no_index');
 
             $table->dropColumn(['wx_transaction_id', 'wx_out_trade_no']);
         });
