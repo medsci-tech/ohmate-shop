e6eb18cade5519635ff6893607df218cd1a043a9
amudogxxx <amudog_1@hotmail.com>
2016-02-29 14:41:50 +0800

    modify

--- resources/views/education/article.blade.php
+++ resources/views/education/article.blade.php
@@ -37,7 +37,7 @@
     <div class="list">
         <div class="row" onclick="updateView('{{$index['id']}}','{{$index['uri']}}')">
             <div class="small-5 columns">
-                <img src="{{$index['thumbnail']}}">
+                <img src="/image/education/titlle-1.png">
             </div>
             <div class="small-7 columns">
                 <p class="list_p">{{$index['title']}}</p>
