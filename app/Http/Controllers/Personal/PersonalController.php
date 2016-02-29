b2532b171be1fe5c59699710ac5f3936da1268b7
ming <1851464029@qq.com>
2016-02-29 14:10:50 +0800

    fix

--- app/Http/Controllers/Personal/PersonalController.php
+++ app/Http/Controllers/Personal/PersonalController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
+use App\Models\CustomerBean;
 use App\Constants\AppConstant;
 use Carbon\Carbon;
 
@@ -28,70 +29,100 @@ class PersonalController extends Controller
         return view('personal.information', ['data' => $data]);
     }
 
+    private function createBeanItem($customerBean)
+    {
+        $day    = sprintf("%02d", $customerBean->updated_at->month) . '-' .
+            sprintf("%02d", $customerBean->updated_at->day);
+        $time   = sprintf("%02d", $customerBean->updated_at->hour) . ':' .
+            sprintf("%02d", $customerBean->updated_at->minute);
+
+        if ($customerBean->result > 0) {
+            $result = '+' . (string)$customerBean->result;
+        } else {
+            $result = '-' . (string)$customerBean->result;
+        } /*else>*/
+
+        $item = array(
+            'day'       => $day,
+            'time'      => $time,
+            'action'    => $customerBean->rate->action_ch,
+            'icons'     => $customerBean->rate->icon_url,
+            'result'    => $result,
+        );
+        return $item;
+    }
+
     public function beans()
     {
-        $customer = \Helper::getCustomer();
-        $customerBeans = $customer->beans;
-        if (!$customerBeans) {
-            return view('personal.no-beans');
-        } /*if>*/
+        $customer       = \Helper::getCustomer();
+        $total          = $customer->beans_total;
+        $beanThisYear   = $customer->beans->where('year(updated_at)', Carbon::now()->year);
 
-        $total = $customer->beans_total;
+        $resultArray = null;
+        foreach ($beanThisYear as $bean) {
+            $item = $this->createBeanItem($bean);
+            $resultArray[$bean->updated_at->month][] = $item;
+        } /*foreach>*/
 
-        $list = array();
-        $lastTitle = null;
-        foreach ($customerBeans as $customerBean) {
+//        $list = array();
+//        $lastTitle = null;
+//        foreach ($customerBeans as $customerBean) {
+//
+//            if ($customerBean->result > 0) {
+//                $result = '+' . (string)$customerBean->result;
+//            } else {
+//                $result = '-' . (string)$customerBean->result;
+//            }
+//
+//            $day = sprintf("%02d", $customerBean->updated_at->month) . '-' .
+//                sprintf("%02d", $customerBean->updated_at->day);
+//            $time = sprintf("%02d", $customerBean->updated_at->hour) . ':' .
+//                sprintf("%02d", $customerBean->updated_at->minute);
+//
+//            $title = (string)$customerBean->updated_at->year . '年'.
+//                (string)$customerBean->updated_at->month . '月账单';
+//
+//            $row = array(
+//                'result'    => $result,
+//                'action'    => $customerBean->rate->action_ch,
+//                'icons'     => $customerBean->rate->icon_url,
+//                'day'       => $day,
+//                'time'      => $time,
+//                'title'     => $title,
+//                'detail'    => $customerBean->detail
+//            );
+//
+//            array_push($list, $row);
+//        }
 
-            if ($customerBean->result > 0) {
-                $result = '+'.(string)$customerBean->result;
-            } else {
-                $result = '-'.(string)$customerBean->result;
-            }
+//        //按月分组
+//        $items = array();
+//        foreach($list as $item) {
+//            $m_title = $item['title'];
+//            unset($item['title']);
+//
+//            if(!isset($items[$m_title])) {
+//                $items[$m_title] = array('title'=>$m_title, 'items'=>array());
+//            }
+//
+//            $items[$m_title]['items'][] = $item;
+//        }
 
-            $day = sprintf("%02d", $customerBean->updated_at->month) . '-' .
-                sprintf("%02d", $customerBean->updated_at->day);
-            $time = sprintf("%02d", $customerBean->updated_at->hour) . ':' .
-                sprintf("%02d", $customerBean->updated_at->minute);
+        return view('personal.beans', [
+            'year'  => Carbon::now()->year,
+            'items' => $resultArray
+        ]);
+    }
 
-            $title = (string)$customerBean->updated_at->year.'年'.
-                (string)$customerBean->updated_at->month.'月账单';
-
-            $row = array(
-                'result'    => $result,
-                'action'    => $customerBean->rate->action_ch,
-                'icons'     => $customerBean->rate->icon_url,
-                'day'       => $day,
-                'time'      => $time,
-                'title'     => $title,
-                'detail'    => $customerBean->detail
-            );
-
-            array_push($list, $row);
-        }
-
-        //按月分组
-        $items = array();
-        foreach($list as $item) {
-            $m_title = $item['title'];
-            unset($item['title']);
-
-            if(!isset($items[$m_title])) {
-                $items[$m_title] = array('title'=>$m_title, 'items'=>array());
-            }
-
-            $items[$m_title]['items'][] = $item;
-        }
-
-        return view('personal.beans', ['total' => $total, 'items' => $items]);
+    public function gifts()
+    {
+        return view('personal.gifts');
     }
 
     public function friend()
     {
-        $customer = \Helper::getCustomer();
-        $data['nickname']           = $customer->nickname;
-        $data['qrCode']             = $customer->qr_code;
-        $data['head_image_url']     = $customer->head_image_url;
-
+        $customer       = \Helper::getCustomer();
+        $data['qrCode'] = $customer->qr_code;
         return view('personal.friend', ['data' => $data]);
     }
 
---
+++ resources/views/personal/gifts.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ming
+ * Date: 2016/2/29
+ * Time: 10:36
+ */
