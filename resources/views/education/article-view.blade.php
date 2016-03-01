8e3ba777bcb410db0608d8b8256862e263f5f32b
amudogxxx <amudog_1@hotmail.com>
2016-03-01 11:14:16 +0800

    modify

--- resources/views/education/article-view.blade.php
+++ resources/views/education/article-view.blade.php
@@ -4,12 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
     <title>文章详情</title>
-    <link rel="stylesheet" href="/css/weui.min.css">
+    <link rel="stylesheet" href="/css/weui.css">
+    <link rel="stylesheet" href="/css/article.css">
     <link rel="stylesheet" href="/css/bonus.css">
 </head>
 <body>
-<div>
-    <p>文章详情</p>
+<div class="weui_panel_bd">
+    <div class="weui_media_box weui_media_text">
+        <h4 class="weui_media_title">{{$article->id}}</h4>
+        <p class="weui_media_desc">时间：{{$article->updated_at->year}}年{{$article->updated_at->month}}月{{$article->updated_at->day}}日</p>
+        <img src="/image/education/xq_1.jpg" width="100%">
+        <img src="/image/education/xq_2.png" width="100%">
+    </div>
 </div>
 <!--BEGIN actionSheet-->
 <div id="actionSheet_wrap">
@@ -22,11 +28,12 @@
         </div>
         <div class="weui_actionsheet_action">
             <p>(每日学习迈豆奖励)</p>
-            <a class="weui_btn weui_btn_default" id="gethongbao">确认领取</a>
+            <input type="button" class="weui_btn weui_btn_default" id="gethongbao" value="确认领取">
         </div>
     </div>
 </div>
 <!--END actionSheet-->
+<input id="text_id" type="hidden" value="{{$article->id}}">
 
 <script src="../../js/vendor/jquery-2.1.4.min.js"></script>
 <script>
@@ -41,6 +48,27 @@
                 $('#mask').addClass('weui_fade_toggle');
                 $('#mask').css('display','block')
                 $('#weui_actionsheet').addClass('weui_actionsheet_toggle');
+
+                $(function () {
+                    var requestUrls = '/education/article/update-bean';
+                    var id = $('#text_id').val();
+                    $.ajax({
+                        url: requestUrls,
+                        data: {
+                            id: id
+                        },
+                        type: "get",
+                        dataType: "json",
+                        success: function (json) {
+
+                        },
+                        error: function (xhr, status, errorThrown) {
+                            alert("Sorry, there was a problem!");
+                        }
+                    });
+
+                });
+
             } else {
                 setTimeout(timer, 1000);
             }
