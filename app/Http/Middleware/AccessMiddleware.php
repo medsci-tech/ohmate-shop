42ea51c319e4ac05b24ecd536b0c1ff1cc991841
蔡乾 <cai.q@foxmail.com>
2016-03-02 17:20:29 +0800

    test double scope

--- app/Http/Middleware/AccessMiddleware.php
+++ app/Http/Middleware/AccessMiddleware.php
@@ -24,6 +24,7 @@ class AccessMiddleware
     {
         try {
             $user       = \Helper::getSessionCachedUser();
+            dd($user);
             $customer   = Customer::where('openid', $user['openid'])->firstOrFail();
 
             if (!$customer->is_registered) {
