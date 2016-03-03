ef997bb6a10652946aa7f0ac83b0016ec5cf3cfe
蔡乾 <cai.q@foxmail.com>
2016-03-03 13:37:42 +0800

    test payment

---
+++ database/migrations/2016_03_03_133429_add_portrait_to_commodities_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPortraitToCommoditiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('commodities', function (Blueprint $table) {
+            $table->string('portrait')->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('commodities', function (Blueprint $table) {
+            $table->dropColumn('portrait');
+        });
+    }
+}
