41dc1d846cc06448abf134b235f553f4590278ca
amudogxxx <amudog_1@hotmail.com>
2016-03-04 18:16:03 +0800

    fix

--- resources/views/education/article-index.blade.php
+++ resources/views/education/article-index.blade.php
@@ -322,7 +322,7 @@
                         $("#" + type).empty();
                         var strHtml = "";
                         $(json.articles).each(function () {
-                            strHtml += "<a href='javascript:void(0);' class='weui_media_box weui_media_appmsg' onclick='updateView('\"" + this.id + "\"')'><div class='weui_media_hd'><img class='weui_media_appmsg_thumb' src='"+pic+"' alt=''></div><div class='weui_media_bd'><h4 class='weui_media_title'>" + this.title + "</h4><p class='weui_media_desc'>" + this.description + "</p></div></a> ";
+                            strHtml += "<a href='javascript:void(0);' class='weui_media_box weui_media_appmsg' onclick='updateView(\"" + this.id + "\")'><div class='weui_media_hd'><img class='weui_media_appmsg_thumb' src='"+pic+"' alt=''></div><div class='weui_media_bd'><h4 class='weui_media_title'>" + this.title + "</h4><p class='weui_media_desc'>" + this.description + "</p></div></a> ";
                             console.log(strHtml);
                         });
                         $("#" + type).html(strHtml);
