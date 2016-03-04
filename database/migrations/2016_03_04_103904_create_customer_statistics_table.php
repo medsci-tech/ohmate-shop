d2e766e70a2e8a50ce34bddb730e87c6d4c164bb
ming <1851464029@qq.com>
2016-03-04 11:14:41 +0800

    add

--- database/migrations/2016_03_04_103904_create_customer_statistics_table.php
+++ database/migrations/2016_03_04_103904_create_customer_statistics_table.php
@@ -17,7 +17,12 @@ class CreateCustomerStatisticsTable extends Migration
             $table->integer('customer_id')->unsigned()->comment('用户');
             $table->foreign('customer_id')->references('id')->on('customers');
 
+            $table->integer('friend_count')->unsigned()->default(0)->comment('好友数');
+            $table->integer('article_count')->unsigned()->default(0)->comment('阅读数');
+            $table->integer('commodity_count')->unsigned()->default(0)->comment('购买商品数');
+            $table->integer('order_count')->unsigned()->default(0)->comment('订单数');
 
+            $table->index('customer_id');
             $table->timestamps();
         });
 
@@ -32,6 +37,7 @@ class CreateCustomerStatisticsTable extends Migration
     {
         Schema::table('customer_statistics', function (Blueprint $table) {
             $table->dropForeign('customer_statistics_customer_id_foreign');
+            $table->dropIndex('customer_id');
         });
         Schema::drop('customer_statistics');
 
