634c2e5d40ef0128fa29cb1c97b2ab82823880c4
蔡乾 <cai.q@foxmail.com>
2016-03-04 16:26:06 +0800

    merge detail front

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -203,7 +203,7 @@ class Order extends Model
      */
     public function queryForDetailPage()
     {
-        return static::where('id',119)->with(['commodities','address'=> function ($query) {
+        return $this->with(['commodities','address'=> function ($query) {
             $query->withTrashed();
         }])->first()->toJson();
     }
