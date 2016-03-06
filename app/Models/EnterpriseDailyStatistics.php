f0bef08536a5ebf73f0c4b9f5c94a82677fa18b9
PythonCakeMing <1851464029@qq.com>
2016-03-06 14:17:21 +0800

    add enterprise statistics

---
+++ app/Models/EnterpriseDailyStatistics.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EnterpriseDailyStatistics extends Model
+{
+    protected $table = 'enterprise_daily_statistics';
+}
