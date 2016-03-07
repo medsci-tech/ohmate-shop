01c7986de5bd3661c9cca2af6016467a995607a7
Icefly <501254061@qq.com>
2016-03-07 16:00:54 +0800

    test

--- resources/views/education/injection.blade.php
+++ resources/views/education/injection.blade.php
@@ -8,7 +8,7 @@
 <body>
 
 <div style="padding: 5px;">
-    <h3 style="width: 100%; text-align: center; font-family: 'Microsoft YaHei'">如何使用胰岛素笔</h3>
+    <h3 style="width: 100%; text-align: center; font-family: 'Microsoft YaHei'">诺和笔5使用指南</h3>
     <video controls="controls"
            poster="/image/education/pen.png"
            src="http://7xrlyr.com1.z0.glb.clouddn.com/anquanzhushe%E8%AF%BA%E5%92%8C%E7%AC%945.mp4"
