d9391ad6c28c60e283321e37930f3077246fe940
Icefly <501254061@qq.com>
2016-03-07 15:25:56 +0800

    test

--- resources/views/education/injection.blade.php
+++ resources/views/education/injection.blade.php
@@ -9,15 +9,15 @@
 
 <div style="padding: 5px; padding-bottom: 56.25%;">
     <video controls="controls"
-           poster="../../image/education/pen.png"
+           poster="/image/education/pen.png"
            src="http://7xrlyr.com1.z0.glb.clouddn.com/anquanzhushe%E8%AF%BA%E5%92%8C%E7%AC%945.mp4"
            width="100%" height="100%"
     >
     </video>
 </div>
-<div style="padding: 5px">
+<div style="padding: 5px; padding-bottom: 56.25%;">
     <video controls="controls"
-           poster="../../image/education/pin.png"
+           poster="/image/education/pin.png"
            src="" width="100%" height="100%"
     >
     </video>
