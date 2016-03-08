d89c0a68b7680321b7cd30d4b36e21a9a3dba353
Icefly <501254061@qq.com>
2016-03-08 23:20:37 +0800

    test

--- resources/views/education/injection.blade.php
+++ resources/views/education/injection.blade.php
@@ -4,25 +4,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
     <title>注射指导</title>
+    <link rel="stylesheet" href="/css/weui.css">
 </head>
-<body>
+<body style=" font-family: 'Microsoft YaHei'">
 
 <div style="padding: 5px;">
-    <h3 style="width: 100%; text-align: center; font-family: 'Microsoft YaHei'">诺和笔5使用指南</h3>
-    <video controls="controls"
-           poster="/image/education/pen.png"
-           src="http://7xrlyr.com1.z0.glb.clouddn.com/anquanzhushe%E8%AF%BA%E5%92%8C%E7%AC%945.mp4"
-           width="100%" height="100%"
-    >
-    </video>
+    <h3 style="width: 100%; text-align: center;">如何使用胰岛素笔</h3>
+    <div class="weui_cells weui_cells_access">
+        <video controls="controls"
+               poster="/image/education/pen.png"
+               src="http://7xrlyr.com1.z0.glb.clouddn.com/anquanzhushe%E8%AF%BA%E5%92%8C%E7%AC%945.mp4"
+               width="100%" height="100%"
+        >
+        </video>
+    </div>
 </div>
+<br>
 <div style="padding: 5px;">
-    <h3 style="width: 100%; text-align: center; font-family: 'Microsoft YaHei'">如何正确使用针头</h3>
-    <video controls="controls"
-           poster="/image/education/pin.png"
-           src="http://7xrlyr.com1.z0.glb.clouddn.com/anquanzhushe%E8%AF%BA%E5%92%8C%E7%AC%945.mp4" width="100%" height="100%"
-    >
-    </video>
+    <h3 style="width: 100%; text-align: center;">如何正确使用针头</h3>
+    <div class="weui_cells weui_cells_access">
+        <a class="weui_cell" href="http://viewer.maka.im/k/0RJHKK9T">
+            <div class="weui_cell_bd weui_cell_primary">
+                <p>胰岛素注射后处理</p>
+            </div>
+            <div class="weui_cell_ft">
+            </div>
+        </a>
+        <a class="weui_cell" href="http://viewer.maka.im/k/4PS5OWJ5">
+            <div class="weui_cell_bd weui_cell_primary">
+                <p>胰岛素注射实施</p>
+            </div>
+            <div class="weui_cell_ft">
+            </div>
+        </a>
+        <a class="weui_cell" href="http://viewer.maka.im/k/VDWDI5K6">
+            <div class="weui_cell_bd weui_cell_primary">
+                <p>胰岛素注射前准备</p>
+            </div>
+            <div class="weui_cell_ft">
+            </div>
+        </a>
+    </div>
 </div>
 
 </body>
