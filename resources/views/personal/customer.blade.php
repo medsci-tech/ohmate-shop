a802f90db18815e3287ed0c376b2a376e2ae8d26
Icefly <501254061@qq.com>
2016-03-08 18:12:19 +0800

    test

--- resources/views/personal/customer.blade.php
+++ resources/views/personal/customer.blade.php
@@ -14,23 +14,23 @@
     <div class="panel-heading">基本统计</div>
     <ul class="list-group">
       <li class="list-group-item">
-        <span class="badge">{{ customerStatistics.friend_count }}</span>
+        <span class="badge">@{{ customerStatistics.friend_count }}</span>
         好友数
       </li>
       <li class="list-group-item">
-        <span class="badge">{{ customerStatistics.artcle_count }}</span>
+        <span class="badge">@{{ customerStatistics.artcle_count }}</span>
         阅读文章数
       </li>
       <li class="list-group-item">
-        <span class="badge">{{ customerStatistics.order_count }}</span>
+        <span class="badge">@{{ customerStatistics.order_count }}</span>
         交易订单数
       </li>
       <li class="list-group-item">
-        <span class="badge">{{ customerStatistics.commodity_count }}</span>
+        <span class="badge">@{{ customerStatistics.commodity_count }}</span>
         购买商品数
       </li>
       <li class="list-group-item">
-        <span class="badge">{{ customerStatistics.money_count }}</span>
+        <span class="badge">@{{ customerStatistics.money_count }}</span>
         总消费金额
       </li>
     </ul>
