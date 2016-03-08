02692b2e351ceb3e2c4460113d0f0d968ddc4bbc
Icefly <501254061@qq.com>
2016-03-08 18:44:04 +0800

    test

--- resources/views/personal/customer.blade.php
+++ resources/views/personal/customer.blade.php
@@ -14,23 +14,23 @@
     <div class="panel-heading">基本统计</div>
     <ul class="list-group">
       <li class="list-group-item">
-        <span class="badge">@{{ customerStatistics.friend_count }}</span>
+        <span class="badge">@{{ customer_statistics.friend_count }}</span>
         好友数
       </li>
       <li class="list-group-item">
-        <span class="badge">@{{ customerStatistics.artcle_count }}</span>
+        <span class="badge">@{{ customer_statistics.artcle_count }}</span>
         阅读文章数
       </li>
       <li class="list-group-item">
-        <span class="badge">@{{ customerStatistics.order_count }}</span>
+        <span class="badge">@{{ customer_statistics.order_count }}</span>
         交易订单数
       </li>
       <li class="list-group-item">
-        <span class="badge">@{{ customerStatistics.commodity_count }}</span>
+        <span class="badge">@{{ customer_statistics.commodity_count }}</span>
         购买商品数
       </li>
       <li class="list-group-item">
-        <span class="badge">@{{ customerStatistics.money_count }}</span>
+        <span class="badge">@{{ customer_statistics.money_count }}</span>
         总消费金额
       </li>
     </ul>
@@ -74,6 +74,12 @@
 <script src="/js/vendor/jquery-2.1.4.min.js"></script>
 <script src="/js/vendor/Chart.min.js"></script>
 <script src="/js/vendor/vue.js"></script>
+<script>
+  var count = new Vue({
+    el: 'costomer_count',
+    data: JSON.parse({{ json_encode($data) }})
+  });
+</script>
 <script src="/js/personal_count.js"></script>
 
 
