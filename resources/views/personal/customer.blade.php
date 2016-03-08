450fe57e7a8fd5ffec86e6ebe0a4d2a38241815b
Icefly <501254061@qq.com>
2016-03-08 22:45:35 +0800

    test

--- resources/views/personal/customer.blade.php
+++ resources/views/personal/customer.blade.php
@@ -105,9 +105,9 @@
     });
     $('.data2').children().eq(i).children('span').css("background-color",color_list[i%5]);
   }
-  
+
   data1.push({ value: .0000001 });
-  data2.push({ value: .0000001 });
+  data2.push({ value: .1 });
 
   Chart.defaults.global.responsive = true;
   var ctx = document.getElementById("study").getContext("2d");
