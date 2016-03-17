f6e7527154e658207895bfb297d3955328d5a8e3
蔡乾 <cai.q@foxmail.com>
2016-03-17 19:44:02 +0800

    add softdeletes to commodities

---
+++ database/migrations/2016_03_17_194235_add_soft_deletes_to_commodities.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSoftDeletesToCommodities extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('commodities', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('commodities', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+    }
+}
