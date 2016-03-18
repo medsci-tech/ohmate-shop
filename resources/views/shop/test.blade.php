684291ba278b0ccdbe408675f1d151ec8abd500d
蔡乾 <cai.q@foxmail.com>
2016-03-18 16:35:41 +0800

    test

--- resources/views/shop/test.blade.php
+++ resources/views/shop/test.blade.php
@@ -76,7 +76,7 @@
             document.form1.detail.value = res.addressDetailInfo;
             document.form1.phone.value = res.telNumber;
         });
-        alert(JSON.stringify(WeixinJSBridge, null, 4));
+        alert(WeixinJSBridge.toSource());
         alert('callback end');
     }
 </script>
