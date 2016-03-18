8383087c1f3828edf45476375954e5d4eda8e555
Icefly <501254061@qq.com>
2016-03-18 15:08:51 +0800

    去掉选择默认地址前端提前显示,  收到后台返回后更改选项

--- public/js/address.js
+++ public/js/address.js
@@ -40,10 +40,10 @@ var list = new Vue({
     },
     chooseAdd: function (e) {
       if (!e.is_default) {
-        for( i=0 ; i<list.addresses.length ; i++){
-          list.addresses[i].is_default = false;
-        }
-        e.is_default = true;
+        //for( i=0 ; i<list.addresses.length ; i++){
+        //  list.addresses[i].is_default = false;
+        //}
+        //e.is_default = true;
         $.post('/shop/address/update',
           {
             id: e.id,
