f164b38452819758355450d77a8284f1bfaf2b9c
蔡乾 <cai.q@foxmail.com>
2016-03-22 10:21:39 +0800

    bugfix

--- app/Http/Controllers/Shop/CartController.php
+++ app/Http/Controllers/Shop/CartController.php
@@ -18,11 +18,18 @@ class CartController extends Controller
     {
         $customer = \Helper::getCustomer();
 
+        $default_address = $customer->addresses()->where('is_default', true)->first();
+
+        if ($default_address) {
+            $default_address = $default_address->toArray();
+            $default_address['post_fee'] = \Helper::getPostFee($default_address->province);
+        }
+
         return response()->json([
             'success' => true,
             'data' => [
                 'beans' => $customer->beans_total,
-                'default_address' => ($default_address = $customer->addresses()->where('is_default', true)->first()),
+                'default_address' => $default_address,
                 'post_fee' => $default_address? \Helper::getPostFee($default_address->province):0
             ]
         ]);
