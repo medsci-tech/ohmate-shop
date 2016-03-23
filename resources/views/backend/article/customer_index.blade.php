17ee7c353d86f9a20ebf6e99050e59287c6b62ee
蔡乾 <cai.q@foxmail.com>
2016-03-23 10:59:23 +0800

    add article blade

--- resources/views/backend/article/customer_index.blade.php
+++ resources/views/backend/article/customer_index.blade.php
@@ -26,7 +26,7 @@
 <div class="uk-grid">
     @foreach($items as $item)
         <div class="uk-width-medium-1-2">
-            <div class="uk-panel">
+            <div class="uk-panel uk-panel-box-primary uk-panel-divider">
                 <div class="uk-panel-badge uk-badge">{{$item->type->type_ch}}</div>
                 <h3 class="uk-panel-title">{{$item->title}}</h3>
             </div>
