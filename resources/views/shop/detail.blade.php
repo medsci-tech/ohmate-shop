d34c72a8d1d3d2865517a6a4ec3d91d16737e42e
蔡乾 <cai.q@foxmail.com>
2016-03-24 23:08:14 +0800

    test

--- resources/views/shop/detail.blade.php
+++ resources/views/shop/detail.blade.php
@@ -46,7 +46,7 @@
       <button class="button button-defualt" :class="goods.storage?'':'disabled'" @click="addGoods()">加入购物车</button>
     </div>
     <div class="col-xs-4">
-      <a href="{{url('/shop/cart')}}" class="button button-caution button-rounded" :class="goods.storage?'':'disabled' @click="addGoods()">立即购买</a>
+      <a href="{{url('/shop/cart')}}" class="button button-caution button-rounded" :class="goods.storage?'':'disabled'" @click="addGoods()">立即购买</a>
     </div>
   </div>
 
