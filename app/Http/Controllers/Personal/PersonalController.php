74344faf2712a2186256a958d28def4c63b8b9e5
蔡乾 <cai.q@foxmail.com>
2016-03-25 11:14:23 +0800

    test

--- app/Http/Controllers/Personal/PersonalController.php
+++ app/Http/Controllers/Personal/PersonalController.php
@@ -120,10 +120,12 @@ class PersonalController extends Controller
     {
         $customer = \Helper::getCustomer();
 
-        if ($customer->id != 17) {
-            return '企业用户目前维护中,请稍等.';
-        }
         if ($customer->type->type_en == AppConstant::CUSTOMER_ENTERPRISE) {
+
+            if ($customer->id != 17) {
+                return '企业用户目前维护中,请稍等.';
+            }
+
             $enterpriseCommodityStatistics = EnterpriseCommodityStatistics::getAllStatistics();
             $enterpriseArticleStatistics = EnterpriseArticleStatistics::getAllStatistics();
 //            $enterpriseBasicStatistics = EnterpriseBasicStatistics::where('date', Carbon::now()->format('Y-m-d'))->get()->toArray();
