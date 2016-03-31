f4c743acb601ebe8b510994352a1872e560aa6e1
蔡乾 <cai.q@foxmail.com>
2016-03-31 11:59:54 +0800

    test

--- app/Http/Controllers/Administrator/CustomerController.php
+++ app/Http/Controllers/Administrator/CustomerController.php
@@ -13,6 +13,11 @@ use App\Http\Controllers\Controller;
 class CustomerController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('backend.customer.index');
