fc78afb2d36bffe6d27b060e7ec2964d2e3e9fba
Icefly <501254061@qq.com>
2016-04-01 17:31:52 +0800

    红包页面优化

--- resources/views/education/hongbao.blade.php
+++ resources/views/education/hongbao.blade.php
@@ -16,6 +16,8 @@
       <div class="weui_actionsheet_menu">
         <img src="{{url('/image/education/hongbao.png')}}" alt="">
         <p>20迈豆</p>
+        <p id="chance"></p>
+        <p id="motto"></p>
       </div>
       <div class="weui_actionsheet_action">
         <a href="{{url('/shop/index')}}" class="weui_btn weui_btn_default" id="getedu">立刻使用</a>
@@ -71,6 +73,18 @@
 
 
   settimer(0);
+</script>
+<script>
+  $(document).ready(function () {
+    var chances = ['今日还可获取4次','今日还可获取3次','今日还可获取2次','今日还可获取1次','今日已获取完毕,明天还来哦~'];
+    var mottos = ['人生在勤，不索何获','书山有路勤为径，学海无涯苦作舟','含泪播种的人一定能含笑收获','行动是成功的阶梯，行动越多，登得越高','今晚多几分钟的准备，明天少几小时的麻烦','拿望远镜看别人，拿放大镜看自己','信心、恒心、决心；创意、乐意','没有天生的信心，只有不断培养的信心','生命对某些人来说是美丽的，这些人的一生都为某个目标而奋斗','做对的事情比把事情做对重要','与其临渊羡鱼，不如退而结网','积极思考造成积极人生，消极思考造成消极人生','以诚感人者，人亦诚而应','先知三日，富贵十年','环境不会改变，解决之道在于改变自己','每一发奋努力的背后，必有加倍的赏赐','不是境况造就人，而是人造就境况','靠山山会倒，靠水水会流，靠自己永远不倒','自古成功在尝试','当一个人先从自己的内心开始奋斗，他就是个有价值的人','生命对某些人来说是美丽的，这些人的一生都为某个目标而奋斗']
+
+    var i = 4;
+    var j =Math.floor(Math.random()*mottos.lenth);
+
+    $('#chance').text(chances[i]);
+    $('#motto').text(motto[j]);
+  })
 </script>
 </body>
 </html>
