ebbbc3ff4514300fb679d907c389b578d0af5909
余鹏飞 <501254061@qq.com>
2016-04-05 16:10:57 +0800

    test

--- public/js/order.js
+++ public/js/order.js
@@ -65,7 +65,7 @@ var order = new Vue({
   methods: {
     choose_data: function (e) {
       var dom = e.currentTarget;
-      var type = e.target.innerHTML.toString();
+      var type = $(e).text();
       console.log(type);
       order.searching.user_type = type;
       if (dom.className != 'active') {
