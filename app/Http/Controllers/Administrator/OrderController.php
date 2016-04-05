baa967897f6348ea60fe70d2398f67b7d90dabeb
蔡乾 <cai.q@foxmail.com>
2016-04-05 10:55:30 +0800

    test

---
+++ app/Http/Controllers/Administrator/OrderController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Administrator;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class OrderController extends Controller
+{
+    public function orderList()
+    {
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'orders' => Order::getPaidOrdersWithRelated()->get()
+            ]
+        ]);
+    }
+}
