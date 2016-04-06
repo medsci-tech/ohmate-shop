d4068c43a94a837ec00f43f23ec7569bc4992862
余鹏飞 <501254061@qq.com>
2016-04-06 13:37:32 +0800

    test

--- resources/views/education/close.blade.php
+++ resources/views/education/close.blade.php
@@ -3,26 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <title></title>
+  <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
   <script>
-    if (typeof WeixinJSBridge == "undefined"){
-      if( document.addEventListener ){
-        document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-      }else if (document.attachEvent){
-        document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
-        document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-      }
-    }else{
-      onBridgeReady();
-    }
-    alert('close页面'+history.length);
-    a = '{{$original_url}}';
-    alert(a);
-    if (a){
-      window.location.href=a;
+    if (a) {
+      window.location.href = a;
     } else {
-      WeixinJSBridge.invoke('closeWindow',{},function(res){
-        //alert(res.err_msg);
-      });
+      wx.closeWindow();
     }
   </script>
 </head>
