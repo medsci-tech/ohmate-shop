1133e03909fb732f9e5665f8a04b0258531b4d25
余鹏飞 <501254061@qq.com>
2016-04-06 13:52:40 +0800

    test

--- resources/views/education/close.blade.php
+++ resources/views/education/close.blade.php
@@ -5,6 +5,31 @@
   <title></title>
   <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
   <script>
+    wx.config({!! $js !!});
+
+    wx.ready(function(){
+      alert('ready');
+      wx.checkJsApi({
+        jsApiList: [
+          'checkJsApi',
+          'editAddress',
+          'chooseWXPay',
+          'getLatestAddress',
+          'openCard',
+          'getLocation'
+        ], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+        success: function(res) {
+          $("#a").click(function () {
+            editAddressCallback();
+          });
+        },
+        fail: function(res) {
+          alert('fail');
+          alert(res);
+        }
+      });
+
+    });
     alert('close页面' + history.length);
     a = '{{$original_url}}';
     alert(a);
