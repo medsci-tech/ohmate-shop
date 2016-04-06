50696efa3faa2e5114488dce8efde75727146472
余鹏飞 <501254061@qq.com>
2016-04-06 13:27:18 +0800

    test

--- resources/views/education/close.blade.php
+++ resources/views/education/close.blade.php
@@ -4,6 +4,16 @@
   <meta charset="UTF-8">
   <title></title>
   <script>
+    if (typeof WeixinJSBridge == "undefined"){
+      if( document.addEventListener ){
+        document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+      }else if (document.attachEvent){
+        document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+        document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+      }
+    }else{
+      onBridgeReady();
+    }
     alert('close页面'+history.length);
     a = '{{$original_url}}';
     alert(a);
