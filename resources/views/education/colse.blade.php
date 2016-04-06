eebd50edc6b74f49ed2424fe98cb963f766edb6c
余鹏飞 <501254061@qq.com>
2016-04-06 12:37:06 +0800

    test

---
+++ resources/views/education/colse.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>迈豆红包</title>
+  <script>
+    history.replaceState('','教育频道','/colse')
+  </script>
+</head>
+<body>
+</body>
