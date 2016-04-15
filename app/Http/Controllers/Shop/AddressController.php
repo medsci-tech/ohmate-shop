5681a451fe17b754736c30397f0374eaca85d591
蔡乾 <cai.q@foxmail.com>
2016-04-15 14:53:47 +0800

    test

--- app/Http/Controllers/Shop/AddressController.php
+++ app/Http/Controllers/Shop/AddressController.php
@@ -148,6 +148,7 @@ class AddressController extends Controller
 
         //先重置所有default
         if ($request->has('is_default') and $request->input('is_default') == true) {
+            echo "123";
             $customer->addresses()->update([
                 'is_default' => false
             ]);
