7b621744b7d2ec0b017d1e026e30f3103768c51f
蔡乾 <cai.q@foxmail.com>
2016-04-15 14:14:32 +0800

    test

--- app/Http/Controllers/Personal/PersonalController.php
+++ app/Http/Controllers/Personal/PersonalController.php
@@ -21,7 +21,7 @@ class PersonalController extends Controller
     function __construct()
     {
         $this->middleware('auth.wechat');
-        $this->middleware('auth.access');
+//        $this->middleware('auth.access');
     }
 
     public function information()
