f92a91fe91079b1d7d385aaa76b50c49c790cbf9
余鹏飞 <501254061@qq.com>
2016-05-05 09:37:23 +0800

    fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -93,7 +93,10 @@ Route::group(['middleware' => 'web'], function () {
     });
 
     Route::group(['prefix' => 'activities'], function () {
-        Route::get('/mothersday', 'ActivitiesController@mothersDay');
+//        Route::get('/mothersday', 'ActivitiesController@mothersDay');
+          Route::get('/mothersday', function(){
+              return view('/activities/mothersday');
+          });
     });
 });
 
