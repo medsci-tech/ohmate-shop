84c82da63b24003a0659123c89a65cae0fdce42f
蔡乾 <cai.q@foxmail.com>
2016-07-14 16:28:23 +0800

    完成migration

---
+++ database/migrations/2016_07_14_162452_add_type_and_level_to_user_information.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTypeAndLevelToUserInformation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customer_informations', function (Blueprint $table) {
+            $table->string('type')->after('customer_id')->default('C');
+            $table->integer('level')->after('type')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customer_informations', function (Blueprint $table) {
+            $table->dropColumn(['type', 'level']);
+        });
+    }
+}
