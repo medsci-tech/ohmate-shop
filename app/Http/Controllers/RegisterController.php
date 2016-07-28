6fd240d4e15d7a960a11b8747770237b0004e4bc
蔡乾 <cai.q@foxmail.com>
2016-07-28 15:02:27 +0800

    test

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -68,6 +68,8 @@ class RegisterController extends Controller
             return redirect()->back()->withErrors($validator)->withInput();
         }
 
+        dd('调试中请稍后');
+
         if ($request->input('code') != $customer->auth_code || $request->input('code') == '000000') {
             return redirect()->back()->with('error_message', '验证码不匹配!')->withInput();
         }
