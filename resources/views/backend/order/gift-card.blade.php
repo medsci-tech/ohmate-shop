fc2333fd8ee56e277cb50bbba1244fcafc2b8cdf
余鹏飞 <501254061@qq.com>
2016-08-08 10:52:21 +0800

    test

--- resources/views/backend/order/gift-card.blade.php
+++ resources/views/backend/order/gift-card.blade.php
@@ -8,11 +8,11 @@
     <div class="row">
       <div class="col-sm-3 col-md-2 sidebar">
         <ul class="nav nav-sidebar">
-          <li :class="(searching.user_type == '未兑换卡券')?'active':''" @click="choose_data" id="unfilled"><a
-            href="#unfilled">未兑换卡券</a></li>
-          <li :class="(searching.user_type == '已兑换卡券')?'active':''" @click="choose_data" id="filled"><a
-            href="#filled">已兑换卡券</a>
-          </li>
+          {{--<li :class="(searching.user_type == '未兑换卡券')?'active':''" @click="choose_data" id="unfilled"><a--}}
+            {{--href="#unfilled">未兑换卡券</a></li>--}}
+          {{--<li :class="(searching.user_type == '已兑换卡券')?'active':''" @click="choose_data" id="filled"><a--}}
+            {{--href="#filled">已兑换卡券</a>--}}
+          {{--</li>--}}
           <li :class="(searching.user_type == '所有卡券')?'active':''" @click="choose_data" id="all"><a href="#all">所有卡券</a>
           </li>
         </ul>
@@ -53,39 +53,6 @@
             </tbody>
           </table>
         </div>
-        <nav class="text-center col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 col-xs-12" id="pagination">
-          <ul class="pagination" @click="choose_page">
-          <li v-if="page_active > 1">
-            <a href="#" aria-label="Previous" name="pre">
-              &laquo;
-            </a>
-          </li>
-          <li v-if="page_show > 1">
-            <a href="#" aria-label="Previous5" name="pre5">
-              &hellip;
-            </a>
-          </li>
-          <li :class="(page_active == page_show)?'active':''"><a href="#">@{{page_show}}</a></li>
-          <li :class="(page_active == page_show+1)?'active':''" v-if="(page_show+1)<=page_all"><a
-              href="#">@{{ page_show+1 }}</a></li>
-          <li :class="(page_active == page_show+2)?'active':''" v-if="(page_show+2)<=page_all"><a
-              href="#">@{{ page_show+2 }}</a></li>
-          <li :class="(page_active == page_show+3)?'active':''" v-if="(page_show+3)<=page_all"><a
-              href="#">@{{ page_show+3 }}</a></li>
-          <li :class="(page_active == page_show+4)?'active':''" v-if="(page_show+4)<=page_all"><a
-              href="#">@{{ page_show+4 }}</a></li>
-          <li v-if="(page_show+5)<=page_all">
-            <a href="#" name="next5" aria-label="Next5">
-              &hellip;
-            </a>
-          </li>
-          <li v-if="page_active < page_all">
-            <a href="#" aria-label="Next" name="next">
-              &raquo;
-            </a>
-          </li>
-          </ul>
-        </nav>
       </div>
       <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main" v-cloak>
         <h2 class="sub-header">@{{ searching.user_type }}<span v-if="searched" class="small">(@{{ searched }})</span>
@@ -110,6 +77,8 @@
             </thead>
             <tbody>
             <tr v-cloak v-for="card in page_data">
+              <td>123</td>
+              <td>123</td>
               <td>123</td>
               <td>123</td>
               <td>123</td>
