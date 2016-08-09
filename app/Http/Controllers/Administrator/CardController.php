136920316ce925a786a694b4e3aaf37a50250a01
蔡乾 <cai.q@foxmail.com>
2016-08-09 12:30:40 +0800

    test

--- app/Http/Controllers/Administrator/CardController.php
+++ app/Http/Controllers/Administrator/CardController.php
@@ -14,6 +14,11 @@ use App\Http\Controllers\Controller;
 class CardController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $result = [];
