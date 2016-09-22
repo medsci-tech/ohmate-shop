7272a1168732791b55b2087c963846db559e79bf
蔡乾 <cai.q@foxmail.com>
2016-09-22 09:56:18 +0800

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -50,7 +50,7 @@ Route::group(['middleware' => 'web'], function () {
 
     Route::group(['prefix' => 'register'], function () {
         Route::get('/create', 'RegisterController@create');
-        Route::post('/store', 'RegisterController@store');
+        Route::any('/store', 'RegisterController@store');
         Route::get('/sms', 'RegisterController@sms');
         Route::get('/error', 'RegisterController@error');
         Route::get('/success', 'RegisterController@success');
