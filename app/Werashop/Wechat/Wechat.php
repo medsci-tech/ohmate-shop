108a9daa9f3daea5d7ecc64fceac17f166b9ee72
lostofland <772932587@qq.com>
2016-11-13 16:25:27 +0800

    修改菜单

--- app/Werashop/Wechat/Wechat.php
+++ app/Werashop/Wechat/Wechat.php
@@ -119,7 +119,8 @@ class Wechat
             (new MenuItem("教育学习"))->buttons([
                 new MenuItem('糖尿病知识', 'view', url('/redirect/article-index')),
                 new MenuItem('安全注射', 'view', url('/education/injection')),
-                new MenuItem('每日活动', 'view', url('/activity/daily')),
+                //new MenuItem('每日活动', 'view', url('/activity/daily')),
+                new MenuItem('糖尿病日', 'view', url('/activity/detail/47711')),
                 neW MenuItem('注册', 'view', url('/register/create'))
             ]),
             (new MenuItem('积分商城', 'view', url('http://puanpharm.ohmate.cn/shop/index?utm_source=puan'))),
